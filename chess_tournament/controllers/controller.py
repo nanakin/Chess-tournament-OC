0be7c2e41eff7800ec84480c6bbd0f1daf03f123
Anna Kaczorowski <kaczoro.anna@gmail.com>
2023-03-21 16:52:30 +0100

    add first draft of the controller

--- chess_tournament/controllers/controller.py
+++ chess_tournament/controllers/controller.py
@@ -1,3 +1,5 @@
+from chess_tournament.views.requests import Request
+from chess_tournament.models.model import Model
 
 
 class Controller:
@@ -5,6 +7,27 @@ class Controller:
     def __init__(self, view, data_path):
         # view
         self.view = view
+        # model
+        self.model = Model(data_path)
 
     def run(self):
-        pass
+        running = True
+        while running:
+            action = self.view.show_main_menu()
+
+            if action == Request.EXIT_APP:
+                running = False
+
+            if action == Request.LAUNCH_PLAYER_MENU:
+                action, action_data = self.view.show_player_registration()
+                if action == Request.EXIT_LOCAL_MENU:
+                    break
+                if action == Request.ADD_PLAYER:
+                    self.model.add_players(action_data)
+
+            if action == Request.LAUNCH_TOURNAMENT_MENU:
+                action, action_data = self.view.show_tournament_registration()
+                if action == Request.EXIT_LOCAL_MENU:
+                    break
+                if action == Request.EXIT_LOCAL_MENU:
+                    self.model.add_tournaments(action_data)
