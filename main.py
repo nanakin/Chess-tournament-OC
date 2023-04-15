7f5691ca5e9e32a018af4d5143028e38837527fe
Anna Kaczorowski <kaczoro.anna@gmail.com>
2023-04-15 21:49:19 +0200

    keep only questionary view

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """Define the chess manager entry point and main function."""
 from chess_tournament.controllers.controller import Controller
+from chess_tournament.views.questionary.view import View
 import argparse
 from pathlib import Path
 
@@ -14,25 +15,6 @@ def main(view_class, data_path):
 if __name__ == "__main__":
     # Program entry point that parses optional argument then call the main function.
     parser = argparse.ArgumentParser(description="Chess Tournament Manager")
-    view_group = parser.add_mutually_exclusive_group()
-    view_group.add_argument(
-        "-c",
-        "--classic",
-        action="store_true",
-        default=False,
-        help="provide a classic input/output user interface",
-    )
-    view_group.add_argument(
-        "-q",
-        "--questionary",
-        action="store_true",
-        default=True,
-        help="provide a questionary user interface",
-    )
     parser.add_argument("-p", "--data-path", default=(Path(".") / "data"))
     args = parser.parse_args()
-    if args.questionary:
-        from chess_tournament.views.questionary.view import View
-    else:
-        from chess_tournament.views.classic.view import View
     main(view_class=View, data_path=args.data_path)
