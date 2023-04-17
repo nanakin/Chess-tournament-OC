06162d3c324c6f4b63e328a13a1a64a79bc2f226
Anna Kaczorowski <kaczoro.anna@gmail.com>
2023-04-17 15:38:17 +0200

    reorder model's methods

--- chess_tournament/models/model.py
+++ chess_tournament/models/model.py
@@ -35,6 +35,8 @@ class Model(BackupManager):
         self.players = {}
         self.tournaments = []
 
+    # -- players --------------------------------------------------------------
+
     @save_at_the_end(players_file=True)
     def add_players(self, *players_data):
         """Create new players and register them in the internal list."""
@@ -90,9 +92,84 @@ class Model(BackupManager):
         """Return the total number of existing players."""
         return len(self.players)
 
+    # -- participants ---------------------------------------------------------
+
+    @save_at_the_end(tournaments_file=True)
+    def add_participants_to_tournament(self, tournament_t, *participants_data):
+        """Create participants from players and link them to the given tournaments."""
+        tournament = self.tournaments[tournament_t]
+        for player_id in participants_data:
+            participant = Participant(self.players[player_id])
+            tournament.participants.append(participant)
+        return str(participant.player), str(tournament)
+
+    @save_at_the_end(tournaments_file=True)
+    def delete_participants_from_tournament(self, tournament_t, *participants_data):
+        """Create participants from players and link them to the given tournaments."""
+        tournament = self.tournaments[tournament_t]
+        for player_id in participants_data:
+            participant_to_log = str(self.players[player_id])
+            for p_index, participant in enumerate(tournament.participants):
+                if participant.player.identifier == player_id:
+                    del tournament.participants[p_index]
+                    break
+        return participant_to_log, str(tournament)
+
     def get_total_participants(self, tournament_t):
         """Return the total number of participant from a given tournament."""
         return len(self.tournaments[tournament_t].participants)
+
+    def get_ordered_participants_str(self, tournament_t):
+        """Return participants descriptions."""
+        sorted_participants = sorted(
+            self.tournaments[tournament_t].participants,
+            key=lambda participant: participant.player,
+        )
+        return [str(participant) for participant in sorted_participants]
+
+    def get_participants_id(self, tournament_t):
+        """Return the participants ID from a given tournament."""
+        return (participant.player.identifier for participant in self.tournaments[tournament_t].participants)
+
+    def _get_winners(self, tournament):
+        """Return strings representation of tournaments winners."""
+        ordered_participants = sorted(tournament.participants)
+        bests = []
+        for i in range(len(ordered_participants)):
+            bests.append(str(ordered_participants[i]))
+            if i >= 2:
+                # in case of tie, continue listing winners with the same score
+                if (len(ordered_participants) > i + 1 and
+                        ordered_participants[i].score == ordered_participants[i + 1].score):
+                    continue
+                break
+        return bests
+
+    # -- matches --------------------------------------------------------------
+
+    @save_at_the_end(tournaments_file=True)
+    def register_score(self, tournament_t, match_m, first_player_result_str):
+        """Register participant score for a given match."""
+        tournament = self.tournaments[tournament_t]
+        round = tournament.current_round
+        first_result = Match.Points[first_player_result_str]
+        pair_result = Match.get_pairs_score_from_first(first_result)
+        round.matches[match_m].register_score(pair_result)
+        are_all_matches_ended = all([bool(match.participants_scores is not None) for match in round.matches])
+        if are_all_matches_ended:
+            round.end_round()
+            if tournament.total_finished_rounds < tournament.total_rounds:
+                tournament.set_next_round()
+        return str(round.matches[match_m])
+
+    @save_at_the_end(tournaments_file=True)
+    def _get_round_matches(self, tournament_t, round_r=None):
+        """Return matches of the given round."""
+        if round_r is None:
+            round_r = max(0, len(self.tournaments[tournament_t].rounds) - 1)
+        if round_r == len(self.tournaments[tournament_t].rounds):
+            self.tournaments[tournament_t].set_next_round()
+        return self.tournaments[tournament_t].get_round_matches(round_r)
 
     def get_total_matches(self, tournament_t, round_r=None):
         """Return the total number of matches from a given tournament’s round."""
@@ -114,13 +191,46 @@ class Model(BackupManager):
             matches.extend(self.get_matches_str(tournament_t, r))
         return matches
 
-    def get_ordered_participants_str(self, tournament_t):
-        """Return participants descriptions."""
-        sorted_participants = sorted(
-            self.tournaments[tournament_t].participants,
-            key=lambda participant: participant.player,
-        )
-        return [str(participant) for participant in sorted_participants]
+    def start_tournament(self, tournament_t):
+        """Start the given tournament (and generate the first round matches)."""
+        matches = self._get_round_matches(tournament_t)
+        return str(self.tournaments[tournament_t]), len(matches)
+
+    def get_matches_str(self, tournament_t, round_r=None):
+        """Return matches descriptions."""
+        if round_r is None:
+            round_r = max(0, len(self.tournaments[tournament_t].rounds) - 1)
+        return [str(match) for match in self.tournaments[tournament_t].get_round_matches(round_r)]
+
+    # -- tournaments ----------------------------------------------------------
+
+    @save_at_the_end(tournaments_file=True)
+    def add_tournaments(self, *tournaments_data):
+        """Create new tournaments and add them to the internal list."""
+        for tournament_data in tournaments_data:
+            tournament_data["name"] = tournament_data["name"].strip()
+            tournament_data["location"] = tournament_data["location"].capitalize()
+            tournament_data["total_rounds"] = int(tournament_data["total_rounds"])
+            if isinstance(tournament_data["begin_date"], str):
+                tournament_data["begin_date"] = date.fromisoformat(tournament_data["begin_date"])
+            if isinstance(tournament_data["end_date"], str):
+                tournament_data["end_date"] = date.fromisoformat(tournament_data["end_date"])
+            if tournament_data["end_date"] < tournament_data["begin_date"]:
+                raise InconsistentDates(f"{tournament_data['name']} ending date ({tournament_data['end_date']})" +
+                                        f" is anterior to starting date ({tournament_data['begin_date']})")
+            tournament = Tournament(**tournament_data)
+            self.tournaments.append(tournament)
+            return str(tournament)
+
+    @save_at_the_end(tournaments_file=True)
+    def start_round(self, tournament_t):
+        """Start the next round of the given tournament."""
+        self.tournaments[tournament_t].start_round()
+
+    def get_ordered_tournaments_str(self):
+        """Return ordered tournaments descriptions."""
+        sorted_tournaments = sorted(self.tournaments)
+        return [str(tournament) for tournament in sorted_tournaments]
 
     def get_tournaments_states_statistics(self):
         """Return tournaments states statistics (number of ongoing/past/future/all)."""
@@ -128,11 +238,6 @@ class Model(BackupManager):
         for filter_name, func_status_filter in self.status_filter.items():
             statistics[filter_name] = sum(1 for tournament in self.tournaments if func_status_filter(tournament))
         return statistics
-
-    def get_ordered_tournaments_str(self):
-        """Return ordered tournaments descriptions."""
-        sorted_tournaments = sorted(self.tournaments)
-        return [str(tournament) for tournament in sorted_tournaments]
 
     def get_total_tournaments(self):
         """Return the total number of tournaments."""
@@ -145,24 +250,6 @@ class Model(BackupManager):
             for t_index, tournament in enumerate(self.tournaments)
             if self.status_filter[status](tournament)
         ]
-
-    def get_participants_id(self, tournament_t):
-        """Return the participants ID from a given tournament."""
-        return (participant.player.identifier for participant in self.tournaments[tournament_t].participants)
-
-    def _get_winners(self, tournament):
-        """Return strings representation of tournaments winners."""
-        ordered_participants = sorted(tournament.participants)
-        bests = []
-        for i in range(len(ordered_participants)):
-            bests.append(str(ordered_participants[i]))
-            if i >= 2:
-                # in case of tie, continue listing winners with the same score
-                if (len(ordered_participants) > i + 1 and
-                        ordered_participants[i].score == ordered_participants[i + 1].score):
-                    continue
-                break
-        return bests
 
     def get_tournament_info(self, tournament_t):
         """Return various tournament data as dictionary."""
@@ -187,82 +274,3 @@ class Model(BackupManager):
             "total_participants": len(tournament.participants),
             "winners": self._get_winners(tournament) if tournament.is_ended else None
         }
-
-    @save_at_the_end(tournaments_file=True)
-    def add_tournaments(self, *tournaments_data):
-        """Create new tournaments and add them to the internal list."""
-        for tournament_data in tournaments_data:
-            tournament_data["name"] = tournament_data["name"].strip()
-            tournament_data["location"] = tournament_data["location"].capitalize()
-            tournament_data["total_rounds"] = int(tournament_data["total_rounds"])
-            if isinstance(tournament_data["begin_date"], str):
-                tournament_data["begin_date"] = date.fromisoformat(tournament_data["begin_date"])
-            if isinstance(tournament_data["end_date"], str):
-                tournament_data["end_date"] = date.fromisoformat(tournament_data["end_date"])
-            if tournament_data["end_date"] < tournament_data["begin_date"]:
-                raise InconsistentDates(f"{tournament_data['name']} ending date ({tournament_data['end_date']})" +
-                                        f" is anterior to starting date ({tournament_data['begin_date']})")
-            tournament = Tournament(**tournament_data)
-            self.tournaments.append(tournament)
-            return str(tournament)
-
-    @save_at_the_end(tournaments_file=True)
-    def add_participants_to_tournament(self, tournament_t, *participants_data):
-        """Create participants from players and link them to the given tournaments."""
-        tournament = self.tournaments[tournament_t]
-        for player_id in participants_data:
-            participant = Participant(self.players[player_id])
-            tournament.participants.append(participant)
-        return str(participant.player), str(tournament)
-
-    @save_at_the_end(tournaments_file=True)
-    def delete_participants_from_tournament(self, tournament_t, *participants_data):
-        """Create participants from players and link them to the given tournaments."""
-        tournament = self.tournaments[tournament_t]
-        for player_id in participants_data:
-            participant_to_log = str(self.players[player_id])
-            for p_index, participant in enumerate(tournament.participants):
-                if participant.player.identifier == player_id:
-                    del tournament.participants[p_index]
-                    break
-        return participant_to_log, str(tournament)
-
-    @save_at_the_end(tournaments_file=True)
-    def register_score(self, tournament_t, match_m, first_player_result_str):
-        """Register participant score for a given match."""
-        tournament = self.tournaments[tournament_t]
-        round = tournament.current_round
-        first_result = Match.Points[first_player_result_str]
-        pair_result = Match.get_pairs_score_from_first(first_result)
-        round.matches[match_m].register_score(pair_result)
-        are_all_matches_ended = all([bool(match.participants_scores is not None) for match in round.matches])
-        if are_all_matches_ended:
-            round.end_round()
-            if tournament.total_finished_rounds < tournament.total_rounds:
-                tournament.set_next_round()
-        return str(round.matches[match_m])
-
-    @save_at_the_end(tournaments_file=True)
-    def start_round(self, tournament_t):
-        """Start the next round of the given tournament."""
-        self.tournaments[tournament_t].start_round()
-
-    def start_tournament(self, tournament_t):
-        """Start the given tournament (and generate the first round matches)."""
-        matches = self._get_round_matches(tournament_t)
-        return str(self.tournaments[tournament_t]), len(matches)
-
-    @save_at_the_end(tournaments_file=True)
-    def _get_round_matches(self, tournament_t, round_r=None):
-        """Return matches of the given round."""
-        if round_r is None:
-            round_r = max(0, len(self.tournaments[tournament_t].rounds) - 1)
-        if round_r == len(self.tournaments[tournament_t].rounds):
-            self.tournaments[tournament_t].set_next_round()
-        return self.tournaments[tournament_t].get_round_matches(round_r)
-
-    def get_matches_str(self, tournament_t, round_r=None):
-        """Return matches descriptions."""
-        if round_r is None:
-            round_r = max(0, len(self.tournaments[tournament_t].rounds) - 1)
-        return [str(match) for match in self.tournaments[tournament_t].get_round_matches(round_r)]
